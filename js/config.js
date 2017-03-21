cb3c4b9cec68b50337fa80b9aadf89921792f556
Wenbo Liu <wwwww818@gmail.com>
2017-03-21 16:58:31 +0800

    Add Handle Status modified by Coxxs/status

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm778634588-02118f06ebd5af6ef5345ab4', //Bysb
+	'm778666917-a1aa85c532b4d7733e1cc128', //CDN
+	'm778634633-baed9b27b327869d7b06c5b0', //cywl
+	'm778634650-5367bd7052a496689df71959', //aofallblog
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
